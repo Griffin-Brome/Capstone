f9d511e01f8c98f7c639676bf13aaa8455ccceab
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2021-03-11 15:13:15 -0800

    Attend to rubocop warnings
    Fix some style issues, namely adding rdoc comments to modules and
    classes

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+##
+# Superclass for all models
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Capstone
+  ##
+  # Application config
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
