b651b598354bf05446d84b9c4c4b5f6fdd3798ea
pat53 <patrickmahler@hotmail.com>
2021-03-14 18:07:31 -0600

    MVC for mailer

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_074517) do
+ActiveRecord::Schema.define(version: 2021_03_14_223815) do
 
   create_table "deadlines", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "login"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
