861b8c321275260c5a43961ad0efa55e12fd996c
pat53 <patrickmahler@hotmail.com>
2021-03-31 11:39:12 -0600

    remove mailer files

--- test/mailers/previews/user_mailer_preview.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
-class UserMailerPreview < ActionMailer::Preview
-  def welcome_email
-    UserMailer.with(user: User.first).welcome_email
-  end
-end
