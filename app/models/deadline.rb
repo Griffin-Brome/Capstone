ef14b41086453f92c333fcedc66791577213ff47
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2021-03-31 18:16:14 -0700

    Implement calendar page
    Uses the 'simple calendar' ruby gem. Nothing too special there, mostly
    just adapted from the README. This is embedded into the 'index' method
    for deadlines, and now the website root '/' is now set to point at the
    calendar.
    
    TODO
    - use current logged in user
    
    Close #24

--- app/models/deadline.rb
+++ app/models/deadline.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
+##
+# Represents a deadline on the calendar
 class Deadline < ApplicationRecord
   has_one :study_preference, dependent: :destroy
 
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
   validates :date, presence: true
+
+  ##
+  # Alias date to start_time so that we can integrate seamlessly with simple
+  # calendar https://github.com/excid3/simple_calendar#rendering-events
+  def start_time
+    date
+  end
 end
--- test/controllers/deadlines_controller_test.rb
+++ test/controllers/deadlines_controller_test.rb
@@ -5,11 +5,18 @@ require 'test_helper'
 class DeadlinesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @deadline = deadlines(:one)
+    @today = Date.current
   end
 
   test 'should get index' do
     get deadlines_url
     assert_response :success
+  end
+
+  test 'should show calendar' do
+    get deadlines_url
+    assert_select 'h2', @today.strftime('%B %Y') # https://ruby-doc.org/stdlib-3.0.0/libdoc/date/rdoc/Date.html#method-i-strftime
+    assert css_select '.deadline'.size.zero?   # Deadlines shouldn't show up yet
   end
 
   test 'should get new' do
@@ -23,6 +30,8 @@ class DeadlinesControllerTest < ActionDispatch::IntegrationTest
            params: { deadline: { name: @deadline.name, description: @deadline.description, date: @deadline.date } }
     end
     assert_redirected_to deadline_url(Deadline.last)
+    get deadlines_url
+    assert css_select '.deadline'.size == 1
   end
 
   test 'should show deadline' do
