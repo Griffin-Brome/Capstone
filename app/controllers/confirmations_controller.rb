609943d4ce7ea78e879c8f9207d9907bad857d11
pat53 <patrickmahler@hotmail.com>
2021-04-10 16:24:52 -0600

    rubocop offences fix

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,35 +1,37 @@
 # frozen_string_literal: true
-  class ConfirmationsController < Devise::ConfirmationsController
-    private 
 
-      def after_confirmation_path_for(resource_name, resource) 
-        sign_in(resource)
-        deadlines_path
-      end
-    # GET /resource/confirmation/new
-    # def new
-    #   super
-    # end
+# mailer confirmation controller
+class ConfirmationsController < Devise::ConfirmationsController
+  private
 
-    # POST /resource/confirmation
-    # def create
-    #   super
-    # end
+  def after_confirmation_path_for(_resource_name, resource)
+    sign_in(resource)
+    deadlines_path
+  end
+  # GET /resource/confirmation/new
+  # def new
+  #   super
+  # end
 
-    # GET /resource/confirmation?confirmation_token=abcdef
-    # def show
-    #   super
-    # end
+  # POST /resource/confirmation
+  # def create
+  #   super
+  # end
 
-    # protected
+  # GET /resource/confirmation?confirmation_token=abcdef
+  # def show
+  #   super
+  # end
 
-    # The path used after resending confirmation instructions.
-    # def after_resending_confirmation_instructions_path_for(resource_name)
-    #   super(resource_name)
-    # end
+  # protected
 
-    # The path used after confirmation.
-    # def after_confirmation_path_for(resource_name, resource)
-    #   super(resource_name, resource)
-    # end
+  # The path used after resending confirmation instructions.
+  # def after_resending_confirmation_instructions_path_for(resource_name)
+  #   super(resource_name)
+  # end
+
+  # The path used after confirmation.
+  # def after_confirmation_path_for(resource_name, resource)
+  #   super(resource_name, resource)
+  # end
 end
